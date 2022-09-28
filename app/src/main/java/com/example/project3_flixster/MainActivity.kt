e7b0711a301cc04750b27078358c9bc2497f04e3
rhyanlugo <rhyanlugocrespo@gmail.com>
2022-09-28 18:03:41 -0400

    Implemented all necessary features and orientation responsivity.

--- app/src/main/java/com/example/project3_flixster/MainActivity.kt
+++ app/src/main/java/com/example/project3_flixster/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.project3_flixster
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.project3_flixster.R.id
 
 class MainActivity : AppCompatActivity()
 {
@@ -9,5 +10,8 @@ class MainActivity : AppCompatActivity()
 	{
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+		val supportFragmentManager = supportFragmentManager
+		val fragmentTransaction = supportFragmentManager.beginTransaction()
+		fragmentTransaction.replace(id.content, MoviesFragment(), null).commit()
 	}
 }
